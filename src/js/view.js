4a5e877782aa493315037f92cc8cbe642bc79bf1
Erick Ochoa <github@ceci-nest-pas.me>
2020-07-16 17:10:54 +0200

    Convert title's html to text
    Stops the title from displaying html entities. Instead, html entities are displayed as the correct characters they represent.

--- src/js/view.js
+++ src/js/view.js
@@ -292,6 +292,7 @@ const Favicon = function(baseHref, follow) {
 const TitleMaxlen = 60, TitleMinlen = 24
 const TitleTruncRe = new RegExp(`([-,.!;:)]\s[^-,.!;:]{0,${TitleMaxlen - TitleMinlen}}|\\s\\S*)$`)
 const TitleTrunc = function(title) {
+  title = html2text(title)
   if (title.length < TitleMaxlen)
     return title
   let res = title.slice(0, TitleMaxlen).match(TitleTruncRe)
