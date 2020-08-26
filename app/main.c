7630a1ed5ce2b4f25041f16a429e73b3397b0a8e
medicinecal <medicinecal@protonmail.com>
2020-08-26 15:39:12 +0000

    Playing with using the webview library as a wrapper around https://fraidyc.at/s/

---
+++ app/main.c
@@ -0,0 +1,16 @@
+// main .c
+#include "webview.h"
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
+                   int nCmdShow) {
+#else
+int main() {
+#endif
+	webview_t w = webview_create(0, NULL);
+	webview_set_title(w, "Webview Example");
+	webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
+	webview_navigate(w, "https://en.m.wikipedia.org/wiki/Main_Page");
+	webview_run(w);
+	webview_destroy(w);
+	return 0;
+}
